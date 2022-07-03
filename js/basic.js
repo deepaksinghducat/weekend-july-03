d8e2092bebeb7e1b4f189b2bfb026422eab4ae20
deepaksinghducat <deepaksinghducat@gmail.com>
2022-07-03 12:49:28 +0530

    upto switch case

---
+++ js/basic.js
@@ -0,0 +1,165 @@
+// console.log('fasfasfd');
+
+// variables
+// var name = "fsafasfd";
+
+// types of variables
+// var  
+// let 
+// const
+
+// 101010101 101010101 
+
+// var types
+
+// var a = 10;
+// var a = 20;
+
+
+// function b () {
+//     var a = 30;
+//     console.log(a);
+// }
+
+// console.log(b());
+// console.log(a);
+
+
+
+// let types
+
+// let name = "fasfasdfasd";
+// name = "fsafsadf";
+// console.log(name);
+
+
+// const type
+// const name = "fasfasdfasfdds";
+// const name = "fsafasfdasfd";
+// console.log(name);
+
+
+
+// Datatypes
+// Number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Array
+// Object
+// typeof
+// Symbol
+
+
+// Number
+
+// var num = 100.12;
+
+// console.log(typeof num);
+
+// BigInt
+
+// Array
+
+// var array = [10,10,40,50,50,"fsafdasfsdaf"];
+
+
+
+// console.log(array[0]);
+
+
+// 100 101 102 103
+
+// 100 + 1 * 0
+
+
+// baseaddress + no of byte occupied by data *  index ;
+
+
+//String
+
+// let name = "fsafasdfasfd";
+// let num = 10;
+// console.log(name[0], num[0]);
+
+// Boolean true /false
+// let a = false;
+// let b = true;
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+// Undefined
+// let a;
+// console.log(a);
+
+// Null
+
+// let a = null;
+
+// let b;
+
+// console.log(a === b);
+
+
+
+// Object
+
+// let array  = ["fafasfd" : "fsafsadfas", "fafasfdfsadfasdf" : "fsafsadfasfdsafadsf"];
+
+// console.log(array);
+
+
+// let obj = {
+//     key1: "fsafsdaffa",
+//     key : "fasfasfd"
+// }
+
+// console.log(obj.key);
+
+// let array  = [{"fafasfd" : "fsafsadfas"},{ "fafasfdfsadfasdf" : "fsafsadfasfdsafadsf"}];
+// console.log(array);
+
+// Operators
+
+
+// types of operators unary, binary , ternary , operands
+
+// var a = b + c;
+
+// Unary 1 operand
+// ++a,a++
+
+// var a = 10;
+// console.log(++a);
+
+// console.log(a);
+
+
+// binary
+// var a = 10
+// var b = 11
+// var c = b + a;
+
+// console.log(c);
+
+// ternary
+// console.log(a == b ? true : false);
+
+
+// var a = 10
+
+//  a += 10;
+//  a = a + 10;
+
+//  console.log(a);
+
+// var a  = "fsafdsa" - 20;
+// console.log(a);
+// console.log(+a);
+
+// var a = "fsafdasf";
+
+// console.log(+a);
---
+++ js/control-structure.js
@@ -0,0 +1,51 @@
+// control structure
+// if
+// else
+// elseif
+// switch
+// while
+// do-while
+// for
+// for in
+// for of 
+// foreach
+
+
+// If 
+// var a = "fsadf";
+
+// if (a == 10 && typeof a == "number") {
+//     console.log("checked");
+// }else if (typeof a == "string") {
+//     console.log("strig");
+// } else {
+//     console.log("not checked");   
+// }
+
+//nested if else
+// var conditon = "fasdfas"
+// if(conditon) {
+//     if(typeof conditon == "number")
+//     {
+//         console.log(" number");
+//     }else{
+//         console.log("not a number");
+//     }
+// }
+
+var conditon = 11;
+
+switch (conditon) {
+    case 10:
+        console.log("fsafas");
+
+        break;
+
+    case 11:
+        console.log("11");
+
+        break;
+    default:
+        console.log("default");
+        break;
+}
